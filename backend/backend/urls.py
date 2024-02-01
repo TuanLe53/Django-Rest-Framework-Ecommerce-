e3a184f42c16b09ec62f552b17249e28dd3a108b
Tuan Le <leanhtuan030523@gmail.com>
2024-02-01 15:24:41 +0700

    Fixed product and order url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("auth_api.urls")),
     path("user/", include("user.urls")),
-    path("/", include("product.urls")),
-    path("/", include("order.urls"))
+    path("", include("product.urls")),
+    path("", include("order.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
