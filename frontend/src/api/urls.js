8fcc8234bfcc62dcac6649cd281919130bf5f0e5
Tuan Le <leanhtuan030523@gmail.com>
2024-02-01 18:56:16 +0700

    Add user url

--- frontend/src/api/urls.js
+++ frontend/src/api/urls.js
@@ -1,7 +1,8 @@
 const URL = {
     Server: "http://127.0.0.1:8000/",
     Product: "http://127.0.0.1:8000/products/",
-    Auth: "http://127.0.0.1:8000/auth/"
+    Auth: "http://127.0.0.1:8000/auth/",
+    User: "http://127.0.0.1:8000/user/",
 }
 
 export default URL
