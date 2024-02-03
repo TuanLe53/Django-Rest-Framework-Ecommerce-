1c22b49589185fb4321d0376c033fe0a65410076
Tuan Le <leanhtuan030523@gmail.com>
2024-02-03 18:53:25 +0700

    Add useEffect to fetch cart and render cart items

--- frontend/src/pages/profile/Cart.jsx
+++ frontend/src/pages/profile/Cart.jsx
@@ -1,7 +1,47 @@
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { Flex } from "antd";
+import { useContext, useEffect, useState } from "react";
+import URL from "../../api/urls";
+import ProductCard from "../../components/ProductCard";
+import AuthContext from "../../contexts/AuthContext";
+
 function Cart() {
+    const { authToken } = useContext(AuthContext);
+    const [cartItems, setCartItems] = useState([]);
+
+    useEffect(() => {
+        const fetchCart = async () => {
+            let res = await fetch(URL.User + "cart/", {
+                headers: {
+                    "Authorization": `Bearer ${String(authToken.access)}`
+                },
+            })
+            let data = await res.json()
+            if (res.status === 200) {
+                setCartItems(data)
+            }
+        }
+
+        fetchCart()
+    }, [])
+
     return (
         <>
-            Cart
+            <h1>Cart <ShoppingCartOutlined /></h1>
+            <Flex
+                gap={20}
+                wrap="wrap"
+            >
+            {cartItems.length === 0 ?
+                <div>
+                    <p>No items found</p>        
+                </div>
+                    :
+                    cartItems.map((item) => (
+                    <ProductCard item={item.product} width={200} key={item.id}/>
+                ))
+            }
+            </Flex>
         </>
     )
 }
