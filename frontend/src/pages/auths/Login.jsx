8a524eec2bc186b09efee76cac21d810aabdd3cd
Tuan Le <leanhtuan030523@gmail.com>
2024-02-22 18:22:59 +0700

    Add styles

--- frontend/src/pages/auths/Login.jsx
+++ frontend/src/pages/auths/Login.jsx
@@ -1,62 +1,70 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Form, Input, Button, Checkbox } from "antd";
+import { Form, Input, Button, Checkbox, Layout } from "antd";
 import AuthContext from "../../contexts/AuthContext";
+import "./auth.css"
+
+const { Content } = Layout;
 
 function Login() {
     const { handleLogin } = useContext(AuthContext);
 
     return (
-        <Form
-            name="login"
-            className="login-form"
-            initialValues={{
-                remember: true,
-            }}
-            onSubmit={e=> e.preventDefault()}
-            onFinish={handleLogin}>
-            <Form.Item
-                name="email"
-                rules={[{
-                    required: true,
-                    message: "Please input your email"
-                }]}
-            >
-                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
-            </Form.Item>
+        <Layout className="authentication">
+            <Content className="authentication-container">
+                <h1 style={{"textAlign": "center"}}>Login</h1>
+                <Form
+                    name="login"
+                    className="login-form"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onSubmit={e=> e.preventDefault()}
+                    onFinish={handleLogin}>
+                    <Form.Item
+                        name="email"
+                        rules={[{
+                            required: true,
+                            message: "Please input your email"
+                        }]}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
+                    </Form.Item>
 
-            <Form.Item
-                name="password"
-                rules={[
-                {
-                    required: true,
-                    message: 'Please input your Password!',
-                },
-                ]}
-            >
-                <Input
-                    prefix={<LockOutlined className="site-form-item-icon" />}
-                    type="password"
-                    placeholder="Password"
-                />
-            </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Password!',
+                        },
+                        ]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
 
-            <Form.Item>
-                <Form.Item name="remember" valuePropName="checked" noStyle>
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
+                    <Form.Item>
+                        <Form.Item name="remember" valuePropName="checked" noStyle>
+                            <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
 
-                <a className="login-form-forgot" href="">Forgot password?</a>
-            </Form.Item>
+                        <a className="login-form-forgot" href="">Forgot password?</a>
+                    </Form.Item>
 
-            <Form.Item>
-                <Button type="primary" htmlType="submit" className="login-form-button">
-                    Log in
-                </Button>
-                Or <Link to="/register">register now!</Link>
-            </Form.Item>
-        </Form>
+                    <Form.Item style={{"float": "right"}}>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                        </Button>
+                        <span> Or <Link to="/register">Register now</Link></span>
+                    </Form.Item>
+                </Form>
+            </Content>
+        </Layout>
     )
 }
 
--- frontend/src/pages/auths/Register.jsx
+++ frontend/src/pages/auths/Register.jsx
@@ -1,6 +1,9 @@
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, Layout } from "antd";
 import URL from "../../api/urls";
 import { useNavigate } from "react-router-dom";
+import "./auth.css"
+
+const { Content } = Layout;
 
 function Register() {
     
@@ -27,71 +30,78 @@ function Register() {
     }
 
     return (
-        <Form
-            name="register"
-            onSubmit={e=> e.preventDefault()}
-            onFinish={handleRegister}
-        >
-            <Form.Item
-                name="email"
-                label="E-mail"
-                rules={[
-                {
-                    type: 'email',
-                    message: 'The input is not valid E-mail!',
-                },
-                {
-                    required: true,
-                    message: 'Please input your E-mail!',
-                },
-                ]}
-            >
-                <Input />
-            </Form.Item>
+        <Layout className="authentication">
+            <Content className="authentication-container">
+                <h1 style={{"textAlign": "center"}}>Register</h1>
+                <Form
+                    name="register"
+                    onSubmit={e=> e.preventDefault()}
+                    onFinish={handleRegister}
+                >
+                    <Form.Item
+                        name="email"
+                        rules={[
+                        {
+                            type: 'email',
+                            message: 'The input is not valid E-mail!',
+                        },
+                        {
+                            required: true,
+                            message: 'Please input your E-mail!',
+                        },
+                        ]}
+                    >
+                        <Input placeholder="Email"/>
+                    </Form.Item>
 
-            <Form.Item
-                name="password"
-                label="Password"
-                rules={[
-                {
-                    required: true,
-                    message: 'Please input your password!',
-                },
-                ]}
-                hasFeedback
-            >
-                <Input.Password />
-            </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                        ]}
+                        hasFeedback
+                    >
+                        <Input.Password placeholder="Password"/>
+                    </Form.Item>
 
-            <Form.Item
-                name="confirm"
-                label="Confirm Password"
-                dependencies={['password']}
-                hasFeedback
-                rules={[
-                {
-                    required: true,
-                    message: 'Please confirm your password!',
-                },
-                ({ getFieldValue }) => ({
-                    validator(_, value) {
-                    if (!value || getFieldValue('password') === value) {
-                        return Promise.resolve();
-                    }
-                    return Promise.reject(new Error('The new password that you entered do not match!'));
-                    },
-                }),
-                ]}
-            >
-                <Input.Password />
-            </Form.Item>
+                    <Form.Item
+                        name="confirm"
+                        dependencies={['password']}
+                        hasFeedback
+                        rules={[
+                        {
+                            required: true,
+                            message: 'Please confirm your password!',
+                        },
+                        ({ getFieldValue }) => ({
+                            validator(_, value) {
+                            if (!value || getFieldValue('password') === value) {
+                                return Promise.resolve();
+                            }
+                            return Promise.reject(new Error('The new password that you entered do not match!'));
+                            },
+                        }),
+                        ]}
+                    >
+                        <Input.Password placeholder="Confirm password"/>
+                    </Form.Item>
 
-            <Form.Item>
-                <Button type="primary" htmlType="submit">
-                    Register
-                </Button>
-            </Form.Item>
-        </Form>
+                    <Form.Item
+                        style={{
+                            "display": "flex",
+                            "justifyContent": "center"
+                        }}
+                    >
+                        <Button type="primary" htmlType="submit">
+                            Register
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Content>
+        </Layout>
     )
 }
 
