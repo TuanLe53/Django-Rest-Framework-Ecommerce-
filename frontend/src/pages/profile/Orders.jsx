d6a322eb14eab658e53cac5ca479c0fa77e4137f
Tuan Le <leanhtuan030523@gmail.com>
2024-02-27 09:02:35 +0700

    Add order

--- frontend/src/pages/profile/Orders.jsx
+++ frontend/src/pages/profile/Orders.jsx
@@ -1,7 +1,41 @@
+import { Card, Flex } from "antd";
+import { Link } from "react-router-dom";
+
+function OrderCard() {
+    
+    return (
+        <Link>
+            <Card
+                title="WN29032ASD"
+                extra="Delivered"
+            >
+                <Flex
+                    justify="space-between"
+                >
+                    <p>Created at: 19-01-2017</p>
+                    <p>Total: 219.000 VND</p>
+                </Flex>
+            </Card>
+        </Link>
+    )
+}
+
 function Orders() {
+    const orders = [1, 2, 3, 4];
+
     return (
         <>
-           Orders
+            <h1>Orders</h1>
+            {orders.length === 0 ?
+            <p>You have no order</p>
+                :
+            <>
+                <p>You have {orders.length} orders</p>
+                {orders.map((order) =>(
+                    <OrderCard key={order} />
+                ))}
+            </>
+            }
         </>
     )
 }
