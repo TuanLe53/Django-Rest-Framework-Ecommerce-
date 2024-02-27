7e6d2b77bca84c4d508868b404529b7499557437
Tuan Le <leanhtuan030523@gmail.com>
2024-02-27 09:13:25 +0700

    Add delete button

--- frontend/src/components/CartItemCard.jsx
+++ frontend/src/components/CartItemCard.jsx
@@ -1,5 +1,6 @@
 import { Card, Flex } from "antd"
 import { useNavigate } from "react-router-dom";
+import { DeleteFilled } from "@ant-design/icons";
 
 function CartItemCard({item}) {
     const product = item.product;
@@ -8,13 +9,14 @@ function CartItemCard({item}) {
 
     return (
         <Card
-            onClick={() => navigate(`/product/${product.id}`, {replace: true})}
             style={{width: "100%", cursor: "pointer"}}
         >
             <Flex
                 justify="space-between"
             >
-                <Flex>
+                <Flex
+                    onClick={() => navigate(`/product/${product.id}`, {replace: true})}
+                >
                     <div style={{width: "100px", height: "100px"}}>
                         <img src={product.images[0]["image"]} style={{height: "100%", width: "100px", objectFit:"contain"}}/>
                     </div>
@@ -28,6 +30,16 @@ function CartItemCard({item}) {
                     <p>Total: {product.price * item.quantity}.000 VND</p>
                 </div>
             </Flex>
+            <DeleteFilled
+                style={{
+                    color: "red",
+                    fontSize: 24,
+                    position: "absolute",
+                    top: 10,
+                    right: 20
+                }}
+                onClick={() => console.log("Delete item")}
+            />
         </Card>
     )
 }
